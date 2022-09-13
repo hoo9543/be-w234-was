cdc15c82d044b187062690fc206259e9d9b36db2
aden-j <aden.j@kakaocorp.com>
2022-09-13 18:00:26 +0900

    request line으로 부터 url 가져오는 함수 구현

--- src/main/java/util/Parsing.java
+++ src/main/java/util/Parsing.java
@@ -1,2 +1,11 @@
-package util;public class Parsing {
+package util;
+
+public class Parsing {
+    public static String getUrlFromRequestLine(String requestLine){
+        try {
+            return requestLine.split(" ")[1];
+        } catch(RuntimeException e){
+            throw new NotFoundUrlException("Can not find URL");
+        }
+    }
 }
