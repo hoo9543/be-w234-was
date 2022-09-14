13e20ea31d48f23830aee7cd75a990e26806d87f
aden-j <aden.j@kakaocorp.com>
2022-09-14 20:59:03 +0900

    User를 저장하는 역할의 controller 구현. view path를 index로 리다이렉트 하고, status code는 200 OK

--- src/main/java/webserver/controller/UserSaveController.java
+++ src/main/java/webserver/controller/UserSaveController.java
@@ -1,18 +1,19 @@
-package webserver;
+package webserver.controller;
 
 import db.Database;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.ModelAndView;
-import util.Request;
-import util.Response;
-import util.StatusCode;
+import webserver.basic.ModelAndView;
+import webserver.basic.Request;
+import webserver.basic.Response;
+import webserver.basic.StatusCode;
 
 import java.util.Map;
 
-public class UserSaveController implements Controller{
+public class UserSaveController implements Controller {
     private static final Logger logger = LoggerFactory.getLogger(UserSaveController.class);
+    private static Database repository = Database.getDatabase();
     @Override
     public ModelAndView process(Request request,Response response){
         Map<String,String> params = request.getParams();
@@ -20,9 +21,9 @@ public class UserSaveController implements Controller{
         ModelAndView modelAndView = new ModelAndView("/index.html");
         modelAndView.getParams().put("User",user);
         response.setStatusCode(StatusCode.FOUND);
+        repository.addUser(user);
 
-        logger.info("------------");
-        logger.info("userId: " + user.getUserId());
+        logger.debug("userId: " + user.getUserId());
 
         return modelAndView;
     }
