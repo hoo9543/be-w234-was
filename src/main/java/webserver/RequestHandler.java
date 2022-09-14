cacbdd56a2be534ddaf0ce6fe8a585e32b962513
aden-j <aden.j@kakaocorp.com>
2022-09-14 21:23:52 +0900

    1. InputStream에서 Data를 가져와 저장하는 Request 객체 생성 2. Request 객체에서 url를 가져와 matching되는 controller 찾음 3. 반환된 model에서 값을 받아와 response 객체에 넣어줌. path 완성시켜서 파일에서 data 읽고 outputStreamd 써줌.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,22 +2,37 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import db.Database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static util.Parsing.*;
-
-//import static util.Parsing.urlFromRequestLine;
+import util.HttpParser;
+import webserver.basic.ModelAndView;
+import webserver.basic.Request;
+import webserver.basic.Response;
+import webserver.controller.Controller;
+import webserver.controller.DefaultController;
+import webserver.controller.UserSaveController;
 
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private Map<String, Controller> urlMappingMap = new HashMap<>();
+    private HttpParser httpParser;
+    private Database database;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        initUrlMappingMap();
+        database = new Database();
+    }
+
+    private void initUrlMappingMap(){
+        urlMappingMap.put("GET /user/create",new UserSaveController());
     }
 
     public void run() {
@@ -26,22 +41,15 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String line = br.readLine();
-            logger.debug("request line : {}", line);
-            String url = urlFromRequestLine(line);
+            HttpParser httpParser = new HttpParser(in,out);
+            Request request = httpParser.getProcessedRequestFromHttpRequest();
+            Response response = new Response();
 
-            while(!"".equals(line)){
-                if (line == null) { return;}
-                line = br.readLine();
-                logger.debug("header : {}", line);
-            }
+            Controller controller = getController(request);
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            //byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            ModelAndView modelAndView = controller.process(request,response);
+            httpParser.setResponseFromModel(modelAndView,request,response);
+            httpParser.writeOutputStreamFromResponse(response);
         } catch (IOException e) {
             logger.error(e.getMessage());
         } catch (RuntimeException e){
@@ -49,23 +57,12 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    private Controller getController(Request request){
+        String str = request.getHttpMethod()+" "+request.getUrl();
+        Controller controller = urlMappingMap.get(str);
+        if (controller == null){
+            controller = new DefaultController();
         }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        return controller;
     }
 }
