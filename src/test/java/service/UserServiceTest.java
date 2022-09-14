ba5eaee1d0d025218753addd98dcd406209c1ee1
aden-j <aden.j@kakaocorp.com>
2022-09-14 22:19:11 +0900

    user service에서 중복된 id로 회원가입 시도할 경우 error 발생

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,2 +1,27 @@
-package service;public class UserServiceTest {
+package service;
+
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static util.Parser.StringDivideAndCheckNum;
+
+public class UserServiceTest {
+
+
+    @Test
+    @DisplayName("동일한 Id의 유저 가입실패 테스트")
+    void UserServiceTest(){
+        UserService userService = new UserService();
+
+        User user1 = new User("user1","pw1","name1","email");
+        User user2 = new User("user1","pw2","name2","email2");
+
+        userService.SignUp(user1);
+
+        assertThatThrownBy(()-> userService.SignUp(user1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("This userId already exists");
+    }
 }
