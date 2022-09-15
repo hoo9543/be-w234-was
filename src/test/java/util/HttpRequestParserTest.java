b18d498bc5b1da61208c576fc0d37574bd484bf7
aden-j <aden.j@kakaocorp.com>
2022-09-15 14:50:03 +0900

    오타 수정

--- src/test/java/util/HttpRequestParserTest.java
+++ src/test/java/util/HttpRequestParserTest.java
@@ -1,12 +1,6 @@
 package util;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import webserver.basic.HttpMethod;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static util.Parser.StringDivideAndCheckNum;
-import static util.Parser.getParamsFromString;
 
 public class HttpRequestParserTest {
 
