da1eb5990e3f0ef897aa067c6c7812beba43422f
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-19 16:46:48 +0900

    MIME type을 관리하기 위한 enum class 작성 및 path 확장자로부터 content type description 가져오는 static함수 구현

--- src/main/java/webserver/http/ContentType.java
+++ src/main/java/webserver/http/ContentType.java
@@ -1,15 +1,20 @@
 package webserver.http;
 
 public enum ContentType {
-    css("text/css"),
-    html("text/html;charset=utf-8");
+    CSS("css","text/css"),
+    HTML("html","text/html;charset=utf-8");
 
+    private final String extension;
     private final String contentType;
 
-    ContentType(String contentType) {
+    ContentType(String extension,String contentType) {
+        this.extension = extension;
         this.contentType = contentType;
     }
 
+    public String getExtension(){
+        return extension;
+    }
     public String getContentType() {
         return contentType;
     }
@@ -17,5 +22,15 @@ public enum ContentType {
     @Override
     public String toString() {
         return "Content-Type: " + contentType;
+    }
+
+    public static String getContentTypeFromPath(String path){
+        String[] splitPath = path.split("\\.");
+        for(ContentType contentType:ContentType.values()){
+            if(contentType.extension.equals(splitPath[splitPath.length-1])){
+                return contentType.contentType;
+            }
+        }
+        return null;
     }
 }
