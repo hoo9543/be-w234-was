c7164920a11bae5c6283aed27465dc2252d40be3
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:55:51 +0900

    UserSaveController의 동작 Test
    1. 회원가입 성공시 redirect 위치와 status code 입력 확인
    2. 회원가입 시도시 Id가 중복 되었을 경우 DuplicatedUserException 발생 확인
    3. request의 user data가 Invalid할 경우 error 확인

--- src/test/java/webserver/controller/UserSaveControllerTest.java
+++ src/test/java/webserver/controller/UserSaveControllerTest.java
@@ -1,2 +1,88 @@
-package webserver.controller;public class UserSaveControllerTest {
+package webserver.controller;
+
+import db.Database;
+import exception.DuplicatedUserIdException;
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.http.HttpMethod;
+import webserver.http.Request;
+import webserver.http.StatusCode;
+import webserver.http.response.Response;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserSaveControllerTest {
+
+    @BeforeEach
+    void clear(){
+        Database.clear();
+    }
+
+    @Test
+    @DisplayName("user 생성 성공 시 redirect 주소와 statuscode 등을 확인")
+    void userSaveSuccessTest() throws IOException {
+
+        Controller controller = new UserSaveController();
+
+        Map<String,String> params = new HashMap<>();
+        params.put("userId","user");
+        params.put("password","pw");
+        params.put("name","name");
+        params.put("email","email");
+
+        Map<String,String> headers = new HashMap<>();
+        Request request = new Request(HttpMethod.GET,"/user/create",params,headers,"","Http/1.1");
+
+        Response response = controller.process(request);
+
+        Assertions.assertThat(response.getHeaders().get("Location")).isEqualTo("/index.html");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(StatusCode.FOUND);
+    }
+
+    @Test
+    @DisplayName("user 생성 실패 시 DuplicatedUserException 발생")
+    void userSaveFailureTest() throws IOException {
+
+        Controller controller = new UserSaveController();
+
+        Map<String,String> params = new HashMap<>();
+        params.put("userId","user");
+        params.put("password","pw");
+        params.put("name","name");
+        params.put("email","email");
+
+        Map<String,String> headers = new HashMap<>();
+        Request request = new Request(HttpMethod.GET,"/user/create",params,headers,"","Http/1.1");
+
+        User user = new User("user","pw","name","email");
+        Database.addUser(user);
+
+
+        assertThatThrownBy(()-> controller.process(request))
+                .isInstanceOf(DuplicatedUserIdException.class)
+                .hasMessage("This userId already exists");
+    }
+
+    @Test
+    @DisplayName("user data가 invalid한 경우 ")
+    void InvalidUserDataTest(){
+        Controller controller = new LoginController();
+        Database.addUser(new User("user","pw","name","email"));
+        Map<String,String> params = new HashMap<>();
+        Map<String,String> headers = new HashMap<>();
+        String body = "userI=user1&password=pw&name=name&email=email";
+        Request request = new Request(HttpMethod.GET,"/user/login",params,headers,body,"Http/1.1");
+
+        assertThatThrownBy(()-> controller.process(request))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("Invalid user data");
+
+    }
 }
