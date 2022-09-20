faf0e7743697b36e5be849d68dfd30fff93a289a
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:25:49 +0900

    UserSaveController 와 동일하게 회원가입 로직을 수행하지만, httpMethod는 post를 사용하고 request body에 user 데이터를 읽어와서 처리함

--- src/main/java/webserver/controller/PostUserSaveController.java
+++ src/main/java/webserver/controller/PostUserSaveController.java
@@ -4,41 +4,34 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
-import webserver.http.ModelAndView;
-import webserver.http.Request;
-import webserver.http.Response;
-import webserver.http.StatusCode;
+import webserver.http.*;
+import webserver.http.response.responseBody.DefaultResponseBody;
+import webserver.http.response.Response;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static util.HttpRequestUtils.parseQueryString;
 
 public class PostUserSaveController implements Controller {
     private static final Logger logger = LoggerFactory.getLogger(UserSaveController.class);
-    private UserService userService = new UserService();
+    private UserService userService = UserService.getInstance();
     @Override
-    public ModelAndView process(Request request, Response response){
+    public Response process(Request request){
 
         User user = getUserFromRequestBody(request.getBody());
+        Response response = new Response(request.getHttpVersion(),StatusCode.FOUND,new DefaultResponseBody());
 
         userService.signUp(user);
+        response.getHeaders().put("Location","/index.html");
 
-        ModelAndView modelAndView = new ModelAndView("/index.html");
-        modelAndView.getParams().put("User",user);
-        response.setStatusCode(StatusCode.FOUND);
-
-        logger.debug("userId: " + user.getUserId());
-
-        return modelAndView;
+        return response;
     }
 
     private User getUserFromRequestBody(String body){
-        try {
-            Map<String, String> userData = parseQueryString(body);
-            return new User(userData.get("userId"), userData.get("password"), userData.get("name"), userData.get("email"));
-        }catch(RuntimeException e){
-            throw new RuntimeException(e);
+        Map<String, String> userData = parseQueryString(body);
+        if (userData.get("userId") == null || userData.get("password") == null || userData.get("name") == null || userData.get("email")== null){
+            throw new RuntimeException("Invalid user data");
         }
+        return new User(userData.get("userId"), userData.get("password"), userData.get("name"), userData.get("email"));
     }
 }
