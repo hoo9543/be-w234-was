b8c55ddd03f62e2618acfdd40a5473b885fb88ea
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 20:08:29 +0900

    login logic 구현
    1. login할 user의 id로 db에서 user를 찾음(찾을 수 없는 경우 NoSuchUserException)
    2. 찾을 경우 비밀번호를 비교(비밀번호 틀린 경우 BadCredientialsException)

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,18 +1,35 @@
 package service;
 
 import db.Database;
+import exception.BadCredentialsException;
+import exception.DuplicatedUserIdException;
+import exception.NoSuchUserException;
 import model.User;
 
 import static db.Database.addUser;
 import static db.Database.findUserById;
 
 public class UserService {
-    //private static Database repository = Database.getDatabase();
+    private UserService(){}
+    private static UserService instance = new UserService();
+    public static UserService getInstance(){ return instance;}
+
 
     public void signUp(User user){
         if (findUserById(user.getUserId()) != null){
-            throw new IllegalArgumentException("This userId already exists");
+            throw new DuplicatedUserIdException("This userId already exists");
         }
         addUser(user);
+    }
+
+    public void login(User loginUser){
+        User user = findUserById(loginUser.getUserId());
+        if (user == null){
+            throw new NoSuchUserException("Failed to find User with id[" +loginUser.getUserId()+"]");
+        }
+        if (!user.getPassword().equals(loginUser.getPassword())){
+            throw new BadCredentialsException("Invalid password");
+        }
+
     }
 }
