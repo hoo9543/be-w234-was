067e155ec539e00bfb040656e4c9870f81683657
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:21:20 +0900

    Login 로직을 수행할 LoginController 구현
    1. login 성공할 경우 response header에 redirect 할 location 위치와 cookie 설정
    2. 실패할 경우 마찬가지로 redirect 할 주소와 login 실패 cookie 남겨줌

--- src/main/java/webserver/controller/LoginController.java
+++ src/main/java/webserver/controller/LoginController.java
@@ -1,2 +1,48 @@
-package webserver.controller;public class LoginController {
+package webserver.controller;
+
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+import webserver.http.*;
+import webserver.http.response.responseBody.DefaultResponseBody;
+import webserver.http.response.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static util.HttpRequestUtils.parseQueryString;
+
+public class LoginController implements Controller{
+
+    private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
+
+    private UserService userService = UserService.getInstance();
+
+    @Override
+    public Response process(Request request) throws IOException {
+
+        User user = getUserFromRequestBody(request.getBody());
+        Response response = new Response(request.getHttpVersion(),StatusCode.FOUND,new DefaultResponseBody());
+
+        try {
+            userService.login(user);
+            response.getHeaders().put("Location","/index.html");
+            response.getHeaders().put("Set-Cookie","logined=true; Path=/");
+        }catch(RuntimeException e){
+            logger.debug(e.getMessage());
+            response.getHeaders().put("Location","/user/login_failed.html");
+            response.getHeaders().put("Set-Cookie","logined=false");
+        }
+
+        return response;
+    }
+
+    private User getUserFromRequestBody(String body){
+            Map<String, String> userData = parseQueryString(body);
+            if (userData.get("userId") == null || userData.get("password") == null || userData.get("name") == null || userData.get("email")== null){
+                throw new RuntimeException("Invalid user data");
+            }
+            return new User(userData.get("userId"), userData.get("password"), userData.get("name"), userData.get("email"));
+    }
 }
