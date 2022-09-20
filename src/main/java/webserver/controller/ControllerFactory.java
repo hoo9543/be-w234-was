5b897950b28a814e1f30cc5d98959d514b769787
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 11:08:30 +0900

    Controller Factory 구현방식 변경 - multiple key(httpMethod,path)를 갖는 hashmap 이용
    1. getController - httpMethod와path를 받아 해당 request를 처리해 줄 수 있는 controller를 찾아 넘겨줌

--- src/main/java/webserver/controller/ControllerFactory.java
+++ src/main/java/webserver/controller/ControllerFactory.java
@@ -1,13 +1,70 @@
 package webserver.controller;
 
 import webserver.http.HttpMethod;
+import webserver.http.Request;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerFactory {
 
-    public Controller getController(HttpMethod httpMethod, String url){
-        if (httpMethod == HttpMethod.GET && url.equals("/user/create")){
-            return new UserSaveController();
+    private Map<Key, Controller> controllerMap= new HashMap<>();
+
+    public ControllerFactory(){
+        controllerMap.put(new Key(HttpMethod.GET,"/user/create"),new UserSaveController());
+        controllerMap.put(new Key(HttpMethod.POST,"/user/create"),new PostUserSaveController());
+    }
+
+    public Controller getController(HttpMethod httpMethod, String path){
+        Controller controller = controllerMap.get(new Key(httpMethod,path));
+        return controller != null ? controller : new DefaultController();
+    }
+
+    class Key<K1, K2>
+    {
+        public K1 key1;
+        public K2 key2;
+
+        public Key(K1 key1, K2 key2)
+        {
+            this.key1 = key1;
+            this.key2 = key2;
         }
-        return new DefaultController();
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Key key = (Key) o;
+            if (key1 != null ? !key1.equals(key.key1) : key.key1 != null) {
+                return false;
+            }
+
+            if (key2 != null ? !key2.equals(key.key2) : key.key2 != null) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = key1 != null ? key1.hashCode() : 0;
+            result = 31 * result + (key2 != null ? key2.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + key1 + ", " + key2 + "]";
+        }
     }
 }
