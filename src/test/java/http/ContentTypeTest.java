ed2c20153f13f7964706b07669385d6940934f41
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:45:12 +0900

    Path의 확장자를 확인하고 해당 ContentType을 return해 주는 getContentTypePath 함수 동작 test

--- src/test/java/http/ContentTypeTest.java
+++ src/test/java/http/ContentTypeTest.java
@@ -1,2 +1,29 @@
-package http;public class ContentTypeTest {
+package http;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.controller.DefaultController;
+import webserver.controller.LoginController;
+import webserver.controller.PostUserSaveController;
+import webserver.controller.UserSaveController;
+import webserver.http.ContentType;
+
+import static webserver.http.ContentType.getContentTypeFromPath;
+
+public class ContentTypeTest {
+
+    @Test
+    @DisplayName("path의 확장자를 확인해서 ContentType return 하는 getContentTypePath 함수 확인")
+    void contentTypeReturnTest(){
+        String cssPath = "/user.css";
+        String htmlPath = "/user.html";
+        String plainText = "/user.txt";
+
+        Assertions.assertThat(getContentTypeFromPath(cssPath)).isEqualTo(ContentType.CSS);
+        Assertions.assertThat(getContentTypeFromPath(htmlPath)).isEqualTo(ContentType.HTML);
+        Assertions.assertThat(getContentTypeFromPath(plainText)).isEqualTo(ContentType.PLAIN_TEXT);
+
+    }
+
 }
