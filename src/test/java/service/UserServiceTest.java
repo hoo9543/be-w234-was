fa2a07300eebeddb227a44f26453269177d5c953
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:45:48 +0900

    UserService의 회원가입과 login logic test
    1. 동일한 Id의 User 회원가입을 시도할 경우 Exception 발생 테스트
    2. login 시 해당 Id의 유저가 없어서 password가 틀렸을 경우 Exception 발생 테스트

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,27 +1,60 @@
 package service;
 
+import db.Database;
+import exception.BadCredentialsException;
+import exception.DuplicatedUserIdException;
+import exception.NoSuchUserException;
 import model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static util.Parser.StringDivideAndCheckNum;
 
 public class UserServiceTest {
 
-
+    @BeforeEach
+    void clear(){
+        Database.clear();
+    }
     @Test
     @DisplayName("동일한 Id의 유저 가입실패 테스트")
-    void UserServiceTest(){
-        UserService userService = new UserService();
+    void signupTest(){
+        UserService userService = UserService.getInstance();
 
         User user1 = new User("user1","pw1","name1","email");
         User user2 = new User("user1","pw2","name2","email2");
 
         userService.signUp(user1);
 
-        assertThatThrownBy(()-> userService.signUp(user1))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(()-> userService.signUp(user2))
+                .isInstanceOf(DuplicatedUserIdException.class)
                 .hasMessage("This userId already exists");
+    }
+
+    @Test
+    @DisplayName("login 시 user가 회원가입 되지 않은 경우 NoSuchUserException 발생 ")
+    void loginNotFoundUserTest(){
+        UserService userService = UserService.getInstance();
+
+        User user1 = new User("user1","pw1","name1","email");
+
+        assertThatThrownBy(()-> userService.login(user1))
+                .isInstanceOf(NoSuchUserException.class)
+                .hasMessage("Failed to find User with id[" +user1.getUserId()+"]");
+    }
+
+    @Test
+    @DisplayName("login 시 user의 비밀번호가 틀린 경우 BadCredentialsException 발생")
+    void loginInvalidPasswordException(){
+        UserService userService = UserService.getInstance();
+        User user1 = new User("user1","pw1","name1","email");
+        User user2 = new User("user1","pw2","name1","email");
+
+        userService.signUp(user1);
+
+        assertThatThrownBy(()-> userService.login(user2))
+                .isInstanceOf(BadCredentialsException.class)
+                .hasMessage("Invalid password");
     }
 }
