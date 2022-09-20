e424acb9c8fa9602a587272ba4065c6766a8932f
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:52:23 +0900

    LoginController의 동작 Test
    1. login 성공시 redirect path와 cookie, status code 등이 잘 입력되었는지 확인
    2. login 실패시 redirect path와 cookie, status code 가 잘 입력되었는지 확인
    3. Request의 User data가 잘못 입력되었을 때 Error 확인

--- src/test/java/webserver/controller/LoginControllerTest.java
+++ src/test/java/webserver/controller/LoginControllerTest.java
@@ -1,2 +1,83 @@
-package webserver.controller;public class LoginControllerTest {
+package webserver.controller;
+
+import db.Database;
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.http.HttpMethod;
+import webserver.http.Request;
+import webserver.http.StatusCode;
+import webserver.http.response.Response;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static util.HttpParser.stringDivideAndCheckNum;
+
+public class LoginControllerTest {
+
+    @BeforeEach
+    void clear(){
+        Database.clear();
+    }
+
+    @Test
+    @DisplayName("login 성공 시 redirect 주소와 cookie, status code가 잘 설정되었는지 확인 ")
+    void loginSuccessTest() throws IOException {
+
+        Controller controller = new LoginController();
+        Database.addUser(new User("user","pw","name","email"));
+        Map<String,String> params = new HashMap<>();
+        Map<String,String> headers = new HashMap<>();
+        String body = "userId=user&password=pw&name=name&email=email";
+        Request request = new Request(HttpMethod.GET,"/user/login",params,headers,body,"Http/1.1");
+
+
+        Response response = controller.process(request);
+
+
+        Assertions.assertThat(response.getHeaders().get("Location")).isEqualTo("/index.html");
+        Assertions.assertThat(response.getHeaders().get("Set-Cookie")).isEqualTo("logined=true; Path=/");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(StatusCode.FOUND);
+    }
+
+    @Test
+    @DisplayName("login 실패 시 redirect 주소와 cookie, status code가 잘 설정되었는지 확인 ")
+    void loginFailureTest() throws IOException {
+
+        Controller controller = new LoginController();
+        Database.addUser(new User("user","pw","name","email"));
+        Map<String,String> params = new HashMap<>();
+        Map<String,String> headers = new HashMap<>();
+        String body = "userId=user1&password=pw&name=name&email=email";
+        Request request = new Request(HttpMethod.GET,"/user/login",params,headers,body,"Http/1.1");
+
+
+        Response response = controller.process(request);
+
+
+        Assertions.assertThat(response.getHeaders().get("Location")).isEqualTo("/user/login_failed.html");
+        Assertions.assertThat(response.getHeaders().get("Set-Cookie")).isEqualTo("logined=false");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(StatusCode.FOUND);
+    }
+
+    @Test
+    @DisplayName("user data가 invalid한 경우 ")
+    void InvalidUserDataTest(){
+        Controller controller = new LoginController();
+        Database.addUser(new User("user","pw","name","email"));
+        Map<String,String> params = new HashMap<>();
+        Map<String,String> headers = new HashMap<>();
+        String body = "userI=user1&password=pw&name=name&email=email";
+        Request request = new Request(HttpMethod.GET,"/user/login",params,headers,body,"Http/1.1");
+
+        assertThatThrownBy(()-> controller.process(request))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("Invalid user data");
+
+    }
 }
