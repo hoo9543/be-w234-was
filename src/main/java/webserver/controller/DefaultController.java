c5f895ad40b6ec4c067dbecd44d232f80ae2fc31
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-20 21:20:58 +0900

    ModelAndView 대신 response 이용

--- src/main/java/webserver/controller/DefaultController.java
+++ src/main/java/webserver/controller/DefaultController.java
@@ -2,18 +2,20 @@ package webserver.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.http.ModelAndView;
-import webserver.http.Request;
-import webserver.http.Response;
+import webserver.http.*;
+import webserver.http.response.responseBody.FileResponseBody;
+import webserver.http.response.Response;
+import webserver.http.response.responseBody.ResponseBody;
+
+import java.io.IOException;
 
 public class DefaultController implements Controller {
     private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
     @Override
-    public ModelAndView process(Request request, Response response) {
-        ModelAndView modelAndView = new ModelAndView(request.getUrl());
-
-        return modelAndView;
+    public Response process(Request request) throws IOException {
+        ResponseBody fileResponseBody = new FileResponseBody(request.getUrl());
+        return new Response(request.getHttpVersion(), StatusCode.OK,fileResponseBody);
     }
 
 }
