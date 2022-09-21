319227cfc94ddf5ad25b79dbf53f4c4dc9f32a51
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-21 15:24:30 +0900

    login 시 user 대신 loginData를 받아서 처리

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -4,10 +4,13 @@ import db.Database;
 import exception.BadCredentialsException;
 import exception.DuplicatedUserIdException;
 import exception.NoSuchUserException;
+import model.LoginData;
 import model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,7 +22,7 @@ public class UserServiceTest {
     }
     @Test
     @DisplayName("동일한 Id의 유저 가입실패 테스트")
-    void signupTest(){
+    void signupTest() throws IOException {
         UserService userService = UserService.getInstance();
 
         User user1 = new User("user1","pw1","name1","email");
@@ -37,7 +40,7 @@ public class UserServiceTest {
     void loginNotFoundUserTest(){
         UserService userService = UserService.getInstance();
 
-        User user1 = new User("user1","pw1","name1","email");
+        LoginData user1 = new LoginData("user1","pw1");
 
         assertThatThrownBy(()-> userService.login(user1))
                 .isInstanceOf(NoSuchUserException.class)
@@ -46,10 +49,10 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("login 시 user의 비밀번호가 틀린 경우 BadCredentialsException 발생")
-    void loginInvalidPasswordException(){
+    void loginInvalidPasswordException() throws IOException {
         UserService userService = UserService.getInstance();
         User user1 = new User("user1","pw1","name1","email");
-        User user2 = new User("user1","pw2","name1","email");
+        LoginData user2 = new LoginData("user1","pw2");
 
         userService.signUp(user1);
 
