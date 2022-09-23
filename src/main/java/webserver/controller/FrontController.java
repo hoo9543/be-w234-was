05a75fd736e07a8fd5fac6f51c608b4519c80ae7
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 15:41:47 +0900

    request를 받아 해당 request를 처리할 수 있는 controller를 찾아 controller로 넘겨주는 역할과 controller에서 exception이 발생했을 때 exception을 처리할 수 있는 exceptionhandler로 넘겨주는 역할을 하는 frontController 구현
    1. ControllerMapper에서 해당 request를 처리할 수 있는 controller를 찾아 request를 처리하도록 함
    
    2. controller에서 exception이 발생했을 경우 exceptionHandlerMapper에서 해당 exception을 처리할 수 있는 exceptionController를 찾아 처리하고 response를 받도록 함
    
    3. checkAuthorization 함수에서 모든 유저가 접근할 수 있는 whiteList와, request의 cookie header를 확인하여 login 여부를 확인하고 접근 권한이 없을 경우 NoAuthorization exception 발생. NoAuthorizationException의 경우는 NoAuthorizationController에서 처리.
    
    checkAuthorization 함수 같은 경우는 따로 class를 만들지 결정하지 못해서 일시적으로 frontcontroller에 두었습니다.

--- src/main/java/webserver/controller/FrontController.java
+++ src/main/java/webserver/controller/FrontController.java
@@ -1,2 +1,57 @@
-package webserver.controller;public class FrontController {
+package webserver.controller;
+
+import db.Database;
+import exception.NoAuthorizationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+import webserver.exceptionController.ExceptionHandlerMapper;
+import webserver.http.request.Request;
+import webserver.http.response.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrontController implements Controller{
+    private static final Logger logger = LoggerFactory.getLogger(FrontController.class);
+    private static ArrayList<String> WHITE_LIST = new ArrayList<>(Arrays.asList("/index.html","/user/create","/user/login"));
+    private static FrontController instance = new FrontController();
+
+    public static FrontController getInstance() {
+        return instance;
+    }
+
+    private ControllerMapper controllerMapper;
+    private ExceptionHandlerMapper exceptionHandlerMapper;
+
+    private FrontController(){
+        controllerMapper = new ControllerMapper();
+        exceptionHandlerMapper = new ExceptionHandlerMapper();
+    }
+    @Override
+    public Response process(Request request) throws IOException{
+
+        try {
+            checkAuthorization(request);
+            Controller controller = controllerMapper.getController(request.getHttpMethod(), request.getUrl());
+            return controller.process(request);
+        }catch(RuntimeException e) {
+            logger.error(e.getMessage());
+            Controller exceptionController = exceptionHandlerMapper.getController(e.getClass());
+            return exceptionController.process(request);
+        }
+
+    }
+
+    private void checkAuthorization(Request request){
+        if (!WHITE_LIST.contains(request.getUrl())) {
+            Map<String, String> cookie = request.getCookies();
+            if (!cookie.get("logined").equals("true")){
+                throw new NoAuthorizationException("Not login");
+            }
+        }
+    }
 }
