13dda42e000827b339d7d04366ac8d7cfa8152c3
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 16:27:52 +0900

    String 형태로 된 constants 들을 저장

--- src/main/java/webserver/http/Constants.java
+++
@@ -1,7 +0,0 @@
-package webserver.http;
-
-public class HttpHeaderString {
-    public static String loginCookie ="logined=true; Path=/";
-    public static String loginFailureCookie = "logined=false";
-    public static String index = "/index.html";
-}
---
+++ src/main/java/webserver/http/StringConstants.java
@@ -0,0 +1,14 @@
+package webserver.http;
+
+public class StringConstants {
+
+    public static String SET_COOKIE = "Set-Cookie";
+    public static String SET_LOCATION = "Location";
+    public static String LOGIN_COOKIE ="logined=true; Path=/";
+    public static String LOGIN_FAILURE_COOKIE = "logined=false";
+    public static String INDEX_PATH = "/index.html";
+    public static String LOGIN_FAILURE_PATH = "/user/login_failed.html";
+    public static String LOGIN_PATH = "user/login";
+
+
+}
