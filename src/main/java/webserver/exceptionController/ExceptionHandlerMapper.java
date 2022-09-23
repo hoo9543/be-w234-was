d7685e49fc3f15d80744ada1b02cb82a45393573
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 16:05:00 +0900

    Exception을 받아 Exception을 처리하기 위한 Controller를 찾아주기 위한 class.
    여러 위치에서 같은 exception이 발생하거나 서로 다른 exception이 같은 응답을 주어야 하는 경우가 있을 것 같아(NoAuthorizationController같은 경우) exception을 처리하는 로직을 기존 controller에서 분리하고 exceptionHandlerMapper를 구현.
    
    default하게 error message와 status code 등을 응답해 주는 controller를 구현할까 생각..

--- src/main/java/webserver/exceptionController/ExceptionHandlerMapper.java
+++ src/main/java/webserver/exceptionController/ExceptionHandlerMapper.java
@@ -1,23 +1,31 @@
-package webserver.controller;
+package webserver.exceptionController;
 
-import exception.NoAuthorizationException;
+import exception.*;
+import webserver.controller.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ExceptionHandlerMapper {
-    private Map<Class<?>,Controller> exceptionHandlerMap = new HashMap<>();
+    private Map<Class<?>, Controller> exceptionHandlerMap = new HashMap<>();
 
-    private static ExceptionHandlerMapper instance = new ExceptionHandlerMapper();
-    public static ExceptionHandlerMapper getInstance(){ return instance;}
+    public ExceptionHandlerMapper(){
+        NoAuthorizationController noAuthorizationController = new NoAuthorizationController();
+        DuplicatedUserController duplicatedUserController = new DuplicatedUserController();
+        LoginFailureController loginFailureController = new LoginFailureController();
 
-    private ExceptionHandlerMapper(){
-        exceptionHandlerMap.put(NoAuthorizationException.class,new NoAuthorizationResponseController());
-
+        exceptionHandlerMap.put(NoAuthorizationException.class,noAuthorizationController);
+        exceptionHandlerMap.put(DuplicatedUserIdException.class,duplicatedUserController);
+        exceptionHandlerMap.put(NoSuchUserException.class,loginFailureController);
+        exceptionHandlerMap.put(BadCredentialsException.class,loginFailureController);
     }
 
     public Controller getController(Class<?> c){
-        return exceptionHandlerMap.get(c);
+        Controller controller = exceptionHandlerMap.get(c);
+        if (controller == null){
+            throw new NotFoundExceptionHandlerException("");
+        }
+        return controller;
     }
 
 }
