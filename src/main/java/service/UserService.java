3573cf6da3e9bcc90746a394e34889e488686240
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 15:39:55 +0900

    User data를 모두 가져오는 findAll 함수 추가

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,2 +1,14 @@
-package service;public interface UserService {
+package service;
+
+import model.LoginData;
+import model.User;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public interface UserService {
+    public void signUp(User user) throws IOException;
+    public void login(LoginData loginData);
+
+    public Collection<User> findAll();
 }
