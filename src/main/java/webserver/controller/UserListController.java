b252fb509e43e73289753d6ed5ae8f9fa5dc7449
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 15:58:24 +0900

    user list를 출력하는 controller 구현

--- src/main/java/webserver/controller/UserListController.java
+++ src/main/java/webserver/controller/UserListController.java
@@ -1,2 +1,35 @@
-package webserver.controller;public class UserListController {
+package webserver.controller;
+
+import db.Database;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+import webserver.http.StatusCode;
+import webserver.http.request.Request;
+import webserver.http.response.Response;
+import webserver.http.response.responseBody.TextResponseBody;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class UserListController implements Controller{
+
+    private static final Logger logger = LoggerFactory.getLogger(UserListController.class);
+    private UserService userService;
+
+    public UserListController(UserService userService){this.userService = userService;}
+
+    @Override
+    public Response process(Request request) throws IOException {
+
+        return new Response(request.getHttpVersion(), StatusCode.OK, new TextResponseBody(getUserString(userService.findAll())));
+    }
+
+    private String getUserString(Collection<User> users) {
+        StringBuilder stringBuilder = new StringBuilder();
+        users.forEach(user -> stringBuilder.append(user.toString()).append("\r\n"));
+        return stringBuilder.toString();
+    }
+
 }
