4e57fff3f7225a2864c1934f436d28479431b22c
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-23 15:55:36 +0900

    trt catch문 제거하고 발생하는 exception은 frontController에서 확인하여 따로 처리하도록 함. location과 cookie, path에 중복하여 들어가는 String의 경우는 Constant class 에서 상수로 만들어 처리

--- src/main/java/webserver/controller/PostUserSaveController.java
+++ src/main/java/webserver/controller/PostUserSaveController.java
@@ -1,38 +1,32 @@
 package webserver.controller;
 
-import exception.DuplicatedUserIdException;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
-import util.UserParser;
+import webserver.http.util.UserParser;
 import webserver.http.*;
 import webserver.http.request.Request;
 import webserver.http.response.responseBody.DefaultResponseBody;
 import webserver.http.response.Response;
-import webserver.http.response.responseBody.TextResponseBody;
 
 import java.io.IOException;
-import java.util.Map;
-
-import static util.HttpRequestUtils.parseQueryString;
 
 public class PostUserSaveController implements Controller {
-    private static final Logger logger = LoggerFactory.getLogger(UserSaveController.class);
-    private UserService userService = UserService.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(PostUserSaveController.class);
+    private UserService userService;
+
+    PostUserSaveController(UserService userService){
+        this.userService = userService;}
     @Override
     public Response process(Request request) throws IOException {
 
         User user = UserParser.getUserFromRequestBody(request.getBody());
-        Response response;
 
-        try {
-            userService.signUp(user);
-            response = new Response(request.getHttpVersion(), StatusCode.FOUND, new DefaultResponseBody());
-            response.getHeaders().put("Location", "/index.html");
-        }catch(DuplicatedUserIdException e) {
-            response = new Response(request.getHttpVersion(), StatusCode.BAD_REQUEST, new TextResponseBody(e.getMessage()));
-        }
+        userService.signUp(user);
+        Response response = new Response(request.getHttpVersion(), StatusCode.FOUND, new DefaultResponseBody());
+        response.setLocation(Constants.INDEX_PATH);
+
         return response;
     }
 
