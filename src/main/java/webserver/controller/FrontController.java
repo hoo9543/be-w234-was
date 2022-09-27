d74b630118a11bd8397631f1b0d6072d8714b604
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:43:02 +0900

    cookie 확인할 때 error 발생하던 것 수정

--- src/main/java/webserver/controller/FrontController.java
+++ src/main/java/webserver/controller/FrontController.java
@@ -1,35 +1,27 @@
 package webserver.controller;
 
-import db.Database;
 import exception.NoAuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
-import webserver.exceptionController.ExceptionHandlerMapper;
+import webserver.exceptionResponseFactory.ExceptionHandler;
 import webserver.http.request.Request;
 import webserver.http.response.Response;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 public class FrontController implements Controller{
     private static final Logger logger = LoggerFactory.getLogger(FrontController.class);
     private static ArrayList<String> WHITE_LIST = new ArrayList<>(Arrays.asList("/index.html","/user/create","/user/login"));
-    private static FrontController instance = new FrontController();
-
-    public static FrontController getInstance() {
-        return instance;
-    }
-
+    
     private ControllerMapper controllerMapper;
-    private ExceptionHandlerMapper exceptionHandlerMapper;
+    private ExceptionHandler exceptionHandler;
 
-    private FrontController(){
-        controllerMapper = new ControllerMapper();
-        exceptionHandlerMapper = new ExceptionHandlerMapper();
+    public FrontController(ControllerMapper controllerMapper, ExceptionHandler exceptionHandler){
+        this.controllerMapper = controllerMapper;
+        this.exceptionHandler = exceptionHandler;
     }
     @Override
     public Response process(Request request) throws IOException{
@@ -40,15 +32,17 @@ public class FrontController implements Controller{
             return controller.process(request);
         }catch(RuntimeException e) {
             logger.error(e.getMessage());
-            Controller exceptionController = exceptionHandlerMapper.getController(e.getClass());
-            return exceptionController.process(request);
+            return exceptionHandler.createResponse(request.getHttpVersion(),e);
         }
 
     }
 
     private void checkAuthorization(Request request){
-        if (!WHITE_LIST.contains(request.getUrl())) {
-            Map<String, String> cookie = request.getCookies();
+        Map<String, String> cookie = request.getCookies();
+        if (request.getUrl().equals("/user/list")) {
+            if (cookie.get("logined") == null){
+                throw new NoAuthorizationException("Not login");
+            }
             if (!cookie.get("logined").equals("true")){
                 throw new NoAuthorizationException("Not login");
             }
