ed0db41071bc3f20789685f58a1b36a04ebd78d0
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 02:31:34 +0900

    constants 추가

--- src/main/java/webserver/http/Constants.java
+++ src/main/java/webserver/http/Constants.java
@@ -2,6 +2,7 @@ package webserver.http;
 
 public class Constants {
 
+    public static String LOGINED = "logined";
     public static String SET_COOKIE = "Set-Cookie";
     public static String SET_LOCATION = "Location";
     public static String LOGIN_COOKIE ="logined=true; Path=/";
@@ -9,6 +10,10 @@ public class Constants {
     public static String INDEX_PATH = "/index.html";
     public static String LOGIN_FAILURE_PATH = "/user/login_failed.html";
     public static String LOGIN_PATH = "/user/login.html";
-
+    public static String USER_CREATE_PATH = "/user/create";
+    public static String USER_LIST_PATH = "/user/list";
+    public static String USER_LOGIN_PATH = "/user/login";
+    public static String BOARD_CREATE_PATH = "/qna/form.html";
+    public static String BOARD_LIST_PATH = "/qna/show";
 
 }
