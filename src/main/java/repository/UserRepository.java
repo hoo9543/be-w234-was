d8498095cd4eb872ebc355f8b5a3ce71076388c9
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:29:35 +0900

    User repository interface

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package repository;public interface UserRepository {
+package repository;
+
+import model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+public interface UserRepository{
+    public void save(User user);
+
+    public User findById(String userId);
+
+    public Collection<User> findAll();
 }
