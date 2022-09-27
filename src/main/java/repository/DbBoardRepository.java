f5979d795920e130b4eb6c942cf0edad39631bba
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:26:27 +0900

    Db에 Board data 를 저장하기 위한 resporitory

--- src/main/java/repository/DbBoardRepository.java
+++ src/main/java/repository/DbBoardRepository.java
@@ -1,2 +1,36 @@
-package repository;public class DbBoardRepository {
+package repository;
+
+import model.Board;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+public class DbBoardRepository implements BoardRepository{
+
+    private final EntityManagerFactory emf;
+    private final EntityManager em;
+    private final EntityTransaction tx;
+
+    public DbBoardRepository(){
+        this.emf = Persistence.createEntityManagerFactory("test");
+        this.em = emf.createEntityManager();
+        this.tx = em.getTransaction();
+    }
+
+    @Override
+    public void save(Board board){
+        try {
+            tx.begin();
+            em.persist(board);
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        }
+    }
+    @Override
+    public Collection<Board> findAll(){
+        return em.createQuery("select b from Board b",Board.class).getResultList();
+    }
 }
+
