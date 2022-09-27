b3d3a41a6c1e8155aea2ec719122ed7e9e1df4aa
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 01:04:21 +0900

    html String을 body로 받는 responseBody

--- src/main/java/webserver/http/response/responseBody/HtmlResponseBody.java
+++ src/main/java/webserver/http/response/responseBody/HtmlResponseBody.java
@@ -1,2 +1,31 @@
-package webserver.http.response.responseBody;public class HtmlResponseBody {
+package webserver.http.response.responseBody;
+
+import webserver.http.ContentType;
+
+import java.io.IOException;
+
+public class HtmlResponseBody implements ResponseBody {
+    ContentType contentType;
+    int contentLength;
+    String body;
+
+    public HtmlResponseBody(String text) throws IOException {
+        this.body = text;
+        this.contentType = ContentType.HTML;
+        this.contentLength = text.length();
+    }
+
+    @Override
+    public byte[] getBody(){
+        return body.getBytes();
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public int getContentLength() {
+        return contentLength;
+    }
 }
