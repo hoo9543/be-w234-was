4f60996f7247c977008d03ba6b1ea8be554cc308
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:35:17 +0900

    MemoryDB 대신UserRepository 사용

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -6,27 +6,35 @@ import exception.DuplicatedUserIdException;
 import exception.NoSuchUserException;
 import model.LoginData;
 import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
 
 import java.io.IOException;
 import java.util.Collection;
 
 import static db.Database.addUser;
-import static db.Database.findUserById;
 
+@Service
 public class UserServiceImpl implements UserService{
 
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Override
     public void signUp(User user) throws IOException {
-        if (findUserById(user.getUserId()) != null){
+        if (userRepository.findById(user.getUserId()) != null){
             throw new DuplicatedUserIdException("This userId already exists");
         }
-        addUser(user);
+        userRepository.save(user);
     }
 
     @Override
     public void login(LoginData loginUser){
-        User user = findUserById(loginUser.getUserId());
-        if (user == null){
+        User user = userRepository.findById(loginUser.getUserId());
+        if (userRepository.findById(loginUser.getUserId()) == null){
             throw new NoSuchUserException("Failed to find User with id[" +loginUser.getUserId()+"]");
         }
         if (!user.getPassword().equals(loginUser.getPassword())){
@@ -37,7 +45,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public Collection<User> findAll(){
-        return Database.findAll();
+        return userRepository.findAll();
 
     }
 }
