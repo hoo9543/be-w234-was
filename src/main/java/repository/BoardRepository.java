108ebca61539b02816cda0bc6cdd4b90bfbf17f9
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:26:03 +0900

    BoardRepository interface

--- src/main/java/repository/BoardRepository.java
+++ src/main/java/repository/BoardRepository.java
@@ -1,2 +1,12 @@
-package repository;public class BoardRepository {
+package repository;
+
+import model.Board;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+public interface BoardRepository {
+
+    public void save(Board board);
+    public Collection<Board> findAll();
 }
