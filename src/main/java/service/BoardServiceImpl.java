64686ce52f385b64e5c609f0c00543ea3016c214
aden.j <aden.j@adenjui-MacBookPro.local>
2022-09-28 00:32:27 +0900

    Board save와 findAll 구현

--- src/main/java/service/BoardServiceImpl.java
+++ src/main/java/service/BoardServiceImpl.java
@@ -1,2 +1,24 @@
-package service;public class BoardServiceImpl {
+package service;
+
+import model.Board;
+import repository.BoardRepository;
+
+import java.util.Collection;
+
+public class BoardServiceImpl implements BoardService{
+
+    private BoardRepository boardRepository;
+    public BoardServiceImpl(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+    @Override
+    public void save(Board board) {
+        boardRepository.save(board);
+    }
+
+    @Override
+    public Collection<Board> findAll() {
+        return boardRepository.findAll();
+    }
 }
